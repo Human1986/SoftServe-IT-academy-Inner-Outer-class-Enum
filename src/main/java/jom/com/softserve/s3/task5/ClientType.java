b57f8358b3da1587a679e7a0079829e1f324e67a
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2024-12-17 22:22:27 +0100

    Task completed

--- src/main/java/jom/com/softserve/s3/task5/ClientType.java
+++ src/main/java/jom/com/softserve/s3/task5/ClientType.java
@@ -1,5 +1,28 @@
 package jom.com.softserve.s3.task5;
 
 enum ClientType {
-	// Write your code here
+    NEW(1),
+    SILVER(12),
+    GOLD(30),
+    PLATINUM(60);
+
+    private int months;
+
+
+    ClientType(int months) {
+        this.months = months;
+    }
+
+    public double discount() {
+
+        return switch (months) {
+            case 1 -> 1.0;
+            case 12 -> (100 - months * 0.25) / 100;
+            case 30 -> (100 - months * 0.3) / 100;
+            case 60 -> (100 - months * 0.35) / 100;
+
+            default -> 0.0;
+        };
+    }
+
 }
