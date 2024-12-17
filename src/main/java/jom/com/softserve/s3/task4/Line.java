9a3f9c74abef98a64815cd9138d6afff680f30a4
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2024-12-17 22:21:00 +0100

    Task completed

--- src/main/java/jom/com/softserve/s3/task4/Line.java
+++ src/main/java/jom/com/softserve/s3/task4/Line.java
@@ -1,7 +1,13 @@
 package jom.com.softserve.s3.task4;
 
 //Describe LineType enum here
+enum LineType {
+    SOLID, DOTTED, DASHED, DOUBLE;
 
+}
 public class Line {
 	// Write method code here
+    public static String drawLine(LineType lineType) {
+        return "The line is " + lineType.name().toLowerCase() + " type";
+    }
 }
