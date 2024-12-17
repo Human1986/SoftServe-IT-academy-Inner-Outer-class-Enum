84519760f669917dd4988b029fbde58d1cd05ead
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2024-12-17 22:18:12 +0100

    Task completed

--- src/main/java/jom/com/softserve/s3/task3/Operation.java
+++ src/main/java/jom/com/softserve/s3/task3/Operation.java
@@ -1,23 +1,47 @@
 package jom.com.softserve.s3.task3;
 
 public class Operation {
-	public static double addAtoB(int a, int b) {
-		// Write your code here
-		return 0;
+	public static double subtractBfromA(int a, int b) {
+		Strategy strategy = new Strategy() {
+			@Override
+			public double doOperation(int a, int b) {
+				return a - b;
+			}
+		};
+		System.out.println(strategy.doOperation(a, b));
+		return strategy.doOperation(a, b);
 	}
 
-	public static double subtractBfromA(int a, int b) {
-		// Write your code here
-		return 0;
+	public static double addAtoB(int a, int b) {
+		Strategy strategy = new Strategy() {
+			@Override
+			public double doOperation(int a, int b) {
+				return a + b;
+			}
+		};
+		System.out.println(strategy.doOperation(a, b));
+		return strategy.doOperation(a, b);
 	}
+	public static double divideAbyB(int a, int b) {
+		Strategy strategy = new Strategy() {
+			@Override
+			public double doOperation(int a, int b) {
+				return a / b;
+			}
+		};
+		System.out.println(strategy.doOperation(a, b));
+		return strategy.doOperation(a, b);
+	}
+
 
 	public static double multiplyAbyB(int a, int b) {
-		// Write your code here
-		return 0;
-	}
-
-	public static double divideAbyB(int a, int b) {
-		// Write your code here
-		return 0;
+		Strategy strategy = new Strategy() {
+			@Override
+			public double doOperation(int a, int b) {
+				return a * b;
+			}
+		};
+		System.out.println(strategy.doOperation(a, b));
+		return strategy.doOperation(a, b);
 	}
 }
