58cf3b8855259aebe8c21e0c473087973db3733d
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2024-12-17 22:15:00 +0100

    Task completed

--- src/main/java/jom/com/softserve/s3/task2/NameList.java
+++ src/main/java/jom/com/softserve/s3/task2/NameList.java
@@ -1,11 +1,31 @@
 package jom.com.softserve.s3.task2;
 
 class NameList {
-	private final String[] names = { "Mike", "Emily", "Nick", "Patric", "Sara" };
+	private final String[] names = {"Mike", "Emily", "Nick", "Patric", "Sara"};
 
-//	public Iterator getIterator() {
-//		return new Iterator();
-//	}
+	public Iterator getIterator() {
+		return new Iterator();
+	}
 
-	// Write your code here
+	public class Iterator implements java.util.Iterator<String> {
+		private int counter = 0;
+		private String[] list;
+
+		private Iterator() {
+			list = names;
+		}
+
+
+		@Override
+		public boolean hasNext() {
+			return counter < list.length;
+		}
+
+		@Override
+		public String next() {
+			int i = counter;
+			counter = i + 1;
+			return list[i];
+		}
+	}
 }
